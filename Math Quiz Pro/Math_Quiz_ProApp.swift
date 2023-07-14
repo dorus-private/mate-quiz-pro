4e3225b5393c2f3b0ce0243fa6b91231a7e1a7cb
Doru Sular <doru.sular@gmail.com>
2023-07-14 08:15:02 +0200

    Initial Commit

---
+++ Math Quiz Pro/Math_Quiz_ProApp.swift
@@ -0,0 +1,17 @@
+//
+//  Math_Quiz_ProApp.swift
+//  Math Quiz Pro
+//
+//  Created by Leon Șular on 14.07.23.
+//
+
+import SwiftUI
+
+@main
+struct Math_Quiz_ProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
