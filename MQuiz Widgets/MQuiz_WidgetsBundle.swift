2dcb7c399ca574270d34c4473ca7ee9f9b8987a3
Doru Sular <doru.sular@gmail.com>
2024-04-19 17:27:53 +0200

    8.0

---
+++ MQuiz Widgets/MQuiz_WidgetsBundle.swift
@@ -0,0 +1,17 @@
+//
+//  MQuiz_WidgetsBundle.swift
+//  MQuiz Widgets
+//
+//  Created by Leon Șular on 18.04.24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MQuiz_WidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        MQuiz_Widgets()
+        MQuiz_WidgetsLiveActivity()
+    }
+}
